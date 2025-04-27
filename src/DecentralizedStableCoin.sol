99dadbfe8680ef336ae573d4b888226fadbe5ffa
Adiel <adielssaad@gmail.com>
2025-04-28 00:48:52 +0300

    feat: implement DecentralizedStableCoin contract
    - Add ERC20Burnable and Ownable functionality
    - Implement mint and burn functions with access control
    - Add custom error handling for amount validation
    - Set up constructor with token name and symbol

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.19;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Adiel Saad
+ * Collateral: Exogenous (ETH, BTC)
+ * Stability: Algorithmic
+ * Relative Stability: Pegged to USD
+ * 
+ * The contract meant to be governed by DSC Engine.
+ * This contract is just the ERC20 implementation of our stablecoin system.
+ */
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__AmountMustBeGreaterThanZero();
+    error DecentralizedStableCoin__AmountExceedsBalance();
+    error DecentralizedStableCoin__InvalidMintRecipient();
+
+    
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeGreaterThanZero();
+        }
+        if (_amount > balance) {
+            revert DecentralizedStableCoin__AmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__InvalidMintRecipient();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__AmountMustBeGreaterThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
